8ee48f5c199be793f4641339d0bf4ef377a9e944
Famwas <160734366+Famwas@users.noreply.github.com>
2025-03-18 12:13:08 +0300

    Create header.jsx

---
+++ header.jsx
@@ -0,0 +1,8 @@
+
+function Header(){
+    return(
+        
+        <img src="https://images.pexels.com/photos/683039/pexels-photo-683039.jpeg?auto=compress&cs=tinysrgb&w=600" alt="coffelistings"></img>     
+    )
+}
+export default Header
